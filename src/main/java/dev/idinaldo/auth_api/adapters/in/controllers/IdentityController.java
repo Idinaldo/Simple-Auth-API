354708938711ad027b09b2fb0e2d325553da9113
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 13:01:13 -0300

    feat: add IdentityController initial implementation to handle HTTP requests related to Identity such as register

---
+++ src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/IdentityController.java
@@ -0,0 +1,30 @@
+package dev.idinaldo.auth_api.adapters.in.controllers;
+
+import dev.idinaldo.auth_api.adapters.in.dtos.ClientIdentityRegisterDTO;
+import dev.idinaldo.auth_api.application.services.IdentityService;
+import dev.idinaldo.auth_api.domain.models.Identity;
+import dev.idinaldo.auth_api.infrastructure.mappers.IdentityMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class IdentityController {
+
+    private final IdentityService identityService;
+    private final IdentityMapper identityMapper;
+
+    public IdentityController(IdentityService identityService, IdentityMapper identityMapper) {
+        this.identityService = identityService;
+        this.identityMapper = identityMapper;
+    }
+
+    public ResponseEntity<String> registerClient(ClientIdentityRegisterDTO identityRegisterDTO) {
+        Identity identity = this.identityMapper.registerDtoToDomain(identityRegisterDTO);
+        this.identityService.registerClient(identity);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Credentials Saved Successfully");
+    }
+}
