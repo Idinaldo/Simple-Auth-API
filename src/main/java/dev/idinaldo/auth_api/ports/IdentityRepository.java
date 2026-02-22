3a9e01c958f06bfb2d89257e658a6994d489f9c7
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 18:40:35 -0300

    refactor: update save method return type

--- src/main/java/dev/idinaldo/auth_api/ports/IdentityRepository.java
+++ src/main/java/dev/idinaldo/auth_api/ports/IdentityRepository.java
@@ -3,7 +3,9 @@ package dev.idinaldo.auth_api.ports;
 import dev.idinaldo.auth_api.domain.models.Identity;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface IdentityRepository {
-    void save(Identity identity);
+    UUID save(Identity identity);
 }
