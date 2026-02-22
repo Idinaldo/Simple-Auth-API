849ba258f62d0bd34fde50c30b0cb76bcbb5e404
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 18:07:22 -0300

    feat: add implementation of registerClient method, converting domain to jpa entity and persisting data in database

--- src/main/java/dev/idinaldo/auth_api/application/services/IdentityService.java
+++ src/main/java/dev/idinaldo/auth_api/application/services/IdentityService.java
@@ -3,14 +3,20 @@ package dev.idinaldo.auth_api.application.services;
 import dev.idinaldo.auth_api.adapters.in.dtos.ClientIdentityRegisterDTO;
 import dev.idinaldo.auth_api.application.usecases.ClientRegisterUseCase;
 import dev.idinaldo.auth_api.domain.models.Identity;
+import dev.idinaldo.auth_api.ports.IdentityRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IdentityService implements ClientRegisterUseCase {
 
+    private final IdentityRepository identityRepository;
+
+    public IdentityService(IdentityRepository identityRepository) {
+        this.identityRepository = identityRepository;
+    }
 
     @Override
     public void registerClient(Identity identity) {
-
+        this.identityRepository.save(identity);
     }
 }
