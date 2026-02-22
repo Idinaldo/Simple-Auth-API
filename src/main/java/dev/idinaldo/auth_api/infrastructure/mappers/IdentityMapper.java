2c3af5eedb8cc2b3c78c2847a2523decbd457043
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 13:00:04 -0300

    feat: add implementation of registerDtoToDomain method to handle mapping between HTTP request and application model

--- src/main/java/dev/idinaldo/auth_api/infrastructure/mappers/IdentityMapper.java
+++ src/main/java/dev/idinaldo/auth_api/infrastructure/mappers/IdentityMapper.java
@@ -1,11 +1,19 @@
 package dev.idinaldo.auth_api.infrastructure.mappers;
 
+import dev.idinaldo.auth_api.adapters.in.dtos.ClientIdentityRegisterDTO;
 import dev.idinaldo.auth_api.domain.models.Identity;
 import dev.idinaldo.auth_api.infrastructure.entities.JpaIdentity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IdentityMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public IdentityMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public JpaIdentity domainToEntity(Identity identity) {
         JpaIdentity jpaIdentity = new JpaIdentity();
@@ -14,5 +22,14 @@ public class IdentityMapper {
         jpaIdentity.setPasswordHash(identity.getPasswordHash());
 
         return jpaIdentity;
+    }
+
+    public Identity registerDtoToDomain(ClientIdentityRegisterDTO identityRegisterDTO) {
+        Identity identity = new Identity();
+
+        identity.setUsername(identityRegisterDTO.username());
+        identity.setPasswordHash(passwordEncoder.encode(identityRegisterDTO.password()));
+
+        return identity;
     }
 }
