d633637ad6ae034ebec468b0993fe8324f4cf93b
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 18:28:14 -0300

    refactor: minor changes

--- src/main/java/dev/idinaldo/auth_api/domain/valueObject/Username.java
+++ src/main/java/dev/idinaldo/auth_api/domain/valueObject/Username.java
@@ -1,21 +1,28 @@
 package dev.idinaldo.auth_api.domain.valueObject;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import dev.idinaldo.auth_api.infrastructure.exceptions.InvalidUsernameException;
 
-public record Username(String value) {
+public record Username(String username) {
 
-    public Username(String value) {
-        if (this.isValid(value)) {
-            this.value = value;
+    @JsonCreator
+    public Username(String username) {
+        if (this.isValid(username)) {
+            this.username = username;
+        } else {
+            throw new InvalidUsernameException();
         }
-        throw new InvalidUsernameException();
     }
 
     private boolean isValid(String value) {
+        System.out.println("[" + value + "]");
+        System.out.println(value.matches("\\w{6,16}"));
         return value.matches("\\w{6,16}");
     }
 
+    @JsonValue
     public String getValue() {
-        return this.value;
+        return this.username;
     }
 }
