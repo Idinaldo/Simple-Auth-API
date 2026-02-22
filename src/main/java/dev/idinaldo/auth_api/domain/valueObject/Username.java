309a39e7e39e866f0c2e3e486c4d9d586933f5d3
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 12:20:32 -0300

    feat: add value object Username to align user's input with business rules

---
+++ src/main/java/dev/idinaldo/auth_api/domain/valueObject/Username.java
@@ -0,0 +1,18 @@
+package dev.idinaldo.auth_api.domain.valueObject;
+
+import dev.idinaldo.auth_api.infrastructure.exceptions.InvalidUsernameException;
+
+public record Username(String value) {
+
+    public Username(String value) {
+        if (this.isValid(value)) {
+            this.value = value;
+        }
+        throw new InvalidUsernameException();
+    }
+
+
+    private boolean isValid(String value) {
+        return value.matches("\\w{6,16}");
+    }
+}
