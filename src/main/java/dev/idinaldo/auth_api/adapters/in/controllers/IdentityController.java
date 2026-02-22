a3dd50653d1519149698c5adfe167d5e70347ef2
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-22 13:08:19 -0300

    feat: add post mapping annotation and define endpoint name

--- src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/IdentityController.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/IdentityController.java
@@ -7,6 +7,7 @@ import dev.idinaldo.auth_api.infrastructure.mappers.IdentityMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ public class IdentityController {
         this.identityMapper = identityMapper;
     }
 
+    @PostMapping("/register")
     public ResponseEntity<String> registerClient(ClientIdentityRegisterDTO identityRegisterDTO) {
         Identity identity = this.identityMapper.registerDtoToDomain(identityRegisterDTO);
         this.identityService.registerClient(identity);
