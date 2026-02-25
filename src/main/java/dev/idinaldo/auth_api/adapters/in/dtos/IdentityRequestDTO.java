4b61ae6d18a750082d6310f23a8ba9b4b3154160
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-25 00:16:01 -0300

    refactor: rename file
    This was necessary since this same DTO can be used in more than one situation

--- src/main/java/dev/idinaldo/auth_api/adapters/in/dtos/IdentityRequestDTO.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/in/dtos/IdentityRequestDTO.java
@@ -2,5 +2,5 @@ package dev.idinaldo.auth_api.adapters.in.dtos;
 
 import dev.idinaldo.auth_api.domain.valueObject.Username;
 
-public record ClientIdentityRegisterDTO(Username username, String password) {
+public record IdentityRequestDTO(Username username, String password) {
 }
