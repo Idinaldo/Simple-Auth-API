bdc2d5c1757e4655f6f4fc774d365d5cb770cec4
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-25 00:21:31 -0300

    feat: add initial structure of Sign In method

--- src/main/java/dev/idinaldo/auth_api/application/services/IdentityService.java
+++ src/main/java/dev/idinaldo/auth_api/application/services/IdentityService.java
@@ -1,24 +1,38 @@
 package dev.idinaldo.auth_api.application.services;
 
-import dev.idinaldo.auth_api.adapters.in.dtos.ClientIdentityRegisterDTO;
 import dev.idinaldo.auth_api.application.usecases.ClientRegisterUseCase;
+import dev.idinaldo.auth_api.application.usecases.SignInUseCase;
 import dev.idinaldo.auth_api.domain.models.Identity;
+import dev.idinaldo.auth_api.infrastructure.entities.JpaIdentity;
 import dev.idinaldo.auth_api.ports.IdentityRepository;
+import dev.idinaldo.auth_api.ports.JwtGenerator;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
-public class IdentityService implements ClientRegisterUseCase {
+public class IdentityService implements ClientRegisterUseCase, SignInUseCase {
 
     private final IdentityRepository identityRepository;
+    private final JwtGenerator jwtGenerator;
+    private final PasswordEncoder passwordEncoder;
 
-    public IdentityService(IdentityRepository identityRepository) {
+    public IdentityService(IdentityRepository identityRepository, JwtGenerator jwtGenerator, PasswordEncoder passwordEncoder) {
         this.identityRepository = identityRepository;
+        this.jwtGenerator = jwtGenerator;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UUID registerClient(Identity identity) {
         return this.identityRepository.save(identity);
+    }
+
+    @Override
+    public String signIn(Identity identity) {
+
+        if (passwordEncoder.matches(identity.getPassword(), userCredentials.getPassword()))
+        return "";
     }
 }
