5ec8549e02b4d23beb4eac70ffd054abbd3ca2ab
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-07 04:07:32 -0300

    feat: add BadRequestException

--- src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
+++ src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
@@ -1,7 +1,8 @@
 package dev.idinaldo.auth_api.application.usecases;
 
 import dev.idinaldo.auth_api.domain.models.Identity;
+import org.apache.coyote.BadRequestException;
 
 public interface SignInUseCase {
-    String signIn(Identity identity);
+    String signIn(Identity identity) throws BadRequestException;
 }
