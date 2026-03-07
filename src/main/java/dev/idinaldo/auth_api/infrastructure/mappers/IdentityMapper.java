1650a6b899b54ffc4697a0bde962ee84a32fbc15
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-07 15:18:03 -0300

    refactor: remove unnecessary methods and update requestDtoToDomain method

--- src/main/java/dev/idinaldo/auth_api/infrastructure/mappers/IdentityMapper.java
+++ src/main/java/dev/idinaldo/auth_api/infrastructure/mappers/IdentityMapper.java
@@ -2,7 +2,6 @@ package dev.idinaldo.auth_api.infrastructure.mappers;
 
 import dev.idinaldo.auth_api.adapters.in.dtos.IdentityRequestDTO;
 import dev.idinaldo.auth_api.domain.models.Identity;
-import dev.idinaldo.auth_api.infrastructure.entities.JpaIdentity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -16,25 +15,10 @@ public class IdentityMapper {
     }
 
     public Identity requestDtoToDomain(IdentityRequestDTO identityRegisterDTO) {
-        return new Identity(identityRegisterDTO.username(), identityRegisterDTO.password());
-    }
-
-    public JpaIdentity domainToEntity(Identity identity) {
-        JpaIdentity jpaIdentity = new JpaIdentity();
-
-        jpaIdentity.setUsername(identity.getUsername().getValue());
-        jpaIdentity.setPasswordHash(this.passwordEncoder.encode(identity.getPassword()));
-
-        return jpaIdentity;
-    }
-
-    public Identity entityToDomain(JpaIdentity jpaIdentity) {
-        Identity identity = new Identity();
-
-        identity.setId(jpaIdentity.getId());
-        identity.setUsernameFromString(jpaIdentity.getUsername());
-
-        return identity;
+        return new Identity(
+                identityRegisterDTO.username(),
+                this.passwordEncoder.encode(identityRegisterDTO.password())
+        );
     }
 
 }
