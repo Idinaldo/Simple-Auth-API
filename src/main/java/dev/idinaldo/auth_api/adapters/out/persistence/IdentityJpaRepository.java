c3a10a516919d485808bfdc39fd1b3282844da47
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-07 15:37:36 -0300

    feat: update findByUsername method to return Optional JpaIdentity

--- src/main/java/dev/idinaldo/auth_api/adapters/out/persistence/IdentityJpaRepository.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/out/persistence/IdentityJpaRepository.java
@@ -5,9 +5,10 @@ import dev.idinaldo.auth_api.infrastructure.entities.JpaIdentity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IdentityJpaRepository extends JpaRepository<JpaIdentity, UUID> {
-    JpaIdentity findByUsername(String username);
+    Optional<JpaIdentity> findByUsername(String username);
 }
