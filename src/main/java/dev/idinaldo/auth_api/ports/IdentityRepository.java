bfd3537b97d553f0b5279c6161298ee98d797de8
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-07 04:09:12 -0300

    feat: add findByUsername method to handle sign in and compare persisted data and input

--- src/main/java/dev/idinaldo/auth_api/ports/IdentityRepository.java
+++ src/main/java/dev/idinaldo/auth_api/ports/IdentityRepository.java
@@ -3,10 +3,11 @@ package dev.idinaldo.auth_api.ports;
 import dev.idinaldo.auth_api.domain.models.Identity;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface IdentityRepository {
     UUID save(Identity identity);
-    Identity findByUsername(String username);
+    Optional<Identity> findByUsername(String username);
 }
