add1b0ca32279ddd1f0d31b21f31d8a5a6752173
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-08 15:04:08 -0300

    feat: add initial implementation of JwtGenerator by adding initial structure of generateToken method

--- src/main/java/dev/idinaldo/auth_api/adapters/out/JwtGeneratorImpl.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/out/JwtGeneratorImpl.java
@@ -1,14 +1,43 @@
 package dev.idinaldo.auth_api.adapters.out;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import dev.idinaldo.auth_api.domain.models.Identity;
 import dev.idinaldo.auth_api.ports.JwtGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import java.util.Arrays;
 
 @Component
 public class JwtGeneratorImpl implements JwtGenerator {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     @Override
     public String generateToken(Identity identity) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("dev.idinaldo.auth-api")
+                    .withSubject(identity.getUsername())
+                    .withIssuedAt(Instant.now())
+                    .withClaim("roles", Arrays.asList(identity.getRoles()))
+                    .withExpiresAt(this.getExpirationDate())
+                    .sign(algorithm);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return "token";
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusMinutes(15).toInstant(ZoneOffset.of("-03:00"));
     }
 }
