c3b71c8ba258ebad33045c55b8c88f8d89761599
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-15 14:05:29 -0300

    feat: add initial implementation of InfoController adding its first endpoint

--- src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/InfoController.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/InfoController.java
@@ -1,4 +1,26 @@
 package dev.idinaldo.auth_api.adapters.in.controllers;
 
+import dev.idinaldo.auth_api.domain.models.Identity;
+import dev.idinaldo.auth_api.ports.IdentityRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/")
 public class InfoController {
+
+    private final IdentityRepository identityRepository;
+
+    public InfoController(IdentityRepository identityRepository) {
+        this.identityRepository = identityRepository;
+    }
+
+    @GetMapping("users")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Identity>> getUsers() {
+        return ResponseEntity.ok(this.identityRepository.findAll());
+    }
 }
