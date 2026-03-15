45f54684a7e5e051db20fadf7faa63f669e009fa
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-15 14:06:26 -0300

    feat: add JWT token validation method to verify access tokens

--- src/main/java/dev/idinaldo/auth_api/adapters/out/JwtGeneratorImpl.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/out/JwtGeneratorImpl.java
@@ -3,6 +3,7 @@ package dev.idinaldo.auth_api.adapters.out;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import dev.idinaldo.auth_api.domain.models.Identity;
+import dev.idinaldo.auth_api.domain.valueObject.RoleName;
 import dev.idinaldo.auth_api.ports.JwtGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,8 @@ import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAmount;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class JwtGeneratorImpl implements JwtGenerator {
@@ -24,17 +27,30 @@ public class JwtGeneratorImpl implements JwtGenerator {
     public String generateToken(Identity identity) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
+            return JWT.create()
                     .withIssuer("dev.idinaldo.auth-api")
                     .withSubject(identity.getUsername())
                     .withIssuedAt(Instant.now())
-                    .withClaim("roles", Arrays.asList(identity.getRoles()))
+                    .withClaim("roles", identity.getRoles().stream().map(RoleName::toString).toList())
                     .withExpiresAt(this.getExpirationDate())
                     .sign(algorithm);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return "token";
+    }
+
+    @Override
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("dev.idinaldo.auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Instant getExpirationDate() {
