fb63920de8635c4a704a7706829e9ebc501499bb
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-15 14:04:25 -0300

    refactor: update response type from String to DTOs to support JSON response format

--- src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/IdentityController.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/in/controllers/IdentityController.java
@@ -1,11 +1,15 @@
 package dev.idinaldo.auth_api.adapters.in.controllers;
 
 import dev.idinaldo.auth_api.adapters.in.dtos.IdentityRequestDTO;
+import dev.idinaldo.auth_api.adapters.out.dtos.RegisterResponseDTO;
+import dev.idinaldo.auth_api.adapters.out.dtos.SignInResponseDTO;
 import dev.idinaldo.auth_api.application.services.IdentityService;
 import dev.idinaldo.auth_api.domain.models.Identity;
 import dev.idinaldo.auth_api.infrastructure.mappers.IdentityMapperFacade;
 import jakarta.validation.Valid;
 import org.apache.coyote.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
-
 @RestController
 @RequestMapping("/auth")
 public class IdentityController {
 
     private final IdentityService identityService;
     private final IdentityMapperFacade identityMapper;
+    private final Logger logger = LoggerFactory.getLogger(IdentityController.class);
 
     public IdentityController(IdentityService identityService, IdentityMapperFacade identityMapper) {
         this.identityService = identityService;
@@ -29,14 +31,13 @@ public class IdentityController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerClient(@RequestBody @Valid IdentityRequestDTO identityRegisterDTO) {
+    public ResponseEntity<RegisterResponseDTO> registerClient(@RequestBody @Valid IdentityRequestDTO identityRegisterDTO) {
         Identity identity = this.identityMapper.requestDtoToDomain(identityRegisterDTO);
-        UUID id = this.identityService.registerClient(identity);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Credentials registered successfully with ID: " + id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.identityService.registerClient(identity));
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<String> signIn(@RequestBody @Valid IdentityRequestDTO identityRequestDTO) throws BadRequestException {
+    public ResponseEntity<SignInResponseDTO> signIn(@RequestBody @Valid IdentityRequestDTO identityRequestDTO) throws BadRequestException {
         return ResponseEntity.ok(this.identityService.signIn(identityRequestDTO));
     }
 }
