62b0dd8e5a202ce8535f8336c0a1c04cbcc6fe64
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-15 14:14:49 -0300

    refactor: update return type

--- src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
+++ src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
@@ -1,8 +1,9 @@
 package dev.idinaldo.auth_api.application.usecases;
 
 import dev.idinaldo.auth_api.adapters.in.dtos.IdentityRequestDTO;
+import dev.idinaldo.auth_api.adapters.out.dtos.SignInResponseDTO;
 import org.apache.coyote.BadRequestException;
 
 public interface SignInUseCase {
-    String signIn(IdentityRequestDTO identityRequestDTO) throws BadRequestException;
+    SignInResponseDTO signIn(IdentityRequestDTO identityRequestDTO) throws BadRequestException;
 }
