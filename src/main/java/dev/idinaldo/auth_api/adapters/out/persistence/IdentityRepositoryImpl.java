ca27e8f200505c1c80dc79275829f78d99a3b313
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-15 14:07:36 -0300

    feat: add findAll to implement admins-only endpoint

--- src/main/java/dev/idinaldo/auth_api/adapters/out/persistence/IdentityRepositoryImpl.java
+++ src/main/java/dev/idinaldo/auth_api/adapters/out/persistence/IdentityRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -32,5 +33,10 @@ public class IdentityRepositoryImpl implements IdentityRepository {
     public Identity findByUsername(String username) throws BadRequestException {
         JpaIdentity jpaIdentity = this.identityJpaRepository.findByUsername(username).orElseThrow(BadRequestException::new);
         return this.identityMapper.entityToDomain(jpaIdentity);
+    }
+
+    @Override
+    public List<Identity> findAll() {
+        return this.identityJpaRepository.findAll().stream().map(identityMapper::entityToDomain).toList();
     }
 }
