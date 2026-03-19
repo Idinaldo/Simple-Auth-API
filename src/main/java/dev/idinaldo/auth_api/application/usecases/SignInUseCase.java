87a29a00e03337a4d0ca3d0265aba47062baabe7
Idinaldo <idinaldooliveira220@gmail.com>
2026-03-19 14:01:19 -0300

    refactor: replace SignInResponseDTO references with AccessTokenDTO

--- src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
+++ src/main/java/dev/idinaldo/auth_api/application/usecases/SignInUseCase.java
@@ -1,9 +1,9 @@
 package dev.idinaldo.auth_api.application.usecases;
 
 import dev.idinaldo.auth_api.adapters.in.dtos.IdentityRequestDTO;
-import dev.idinaldo.auth_api.adapters.out.dtos.SignInResponseDTO;
+import dev.idinaldo.auth_api.adapters.out.dtos.AccessTokenDTO;
 import org.apache.coyote.BadRequestException;
 
 public interface SignInUseCase {
-    SignInResponseDTO signIn(IdentityRequestDTO identityRequestDTO) throws BadRequestException;
+    AccessTokenDTO signIn(IdentityRequestDTO identityRequestDTO) throws BadRequestException;
 }
